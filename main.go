183889216baeb6f565c256a5471304543a09e882
efortin <fortin.emmanuel@gmail.com>
2019-02-10 07:24:29 +0100

    Upgrade glide Add prometheus handler

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/ca-gip/kubi/services"
 	"github.com/ca-gip/kubi/utils"
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog/log"
 	"net/http"
 	"os"
@@ -29,6 +31,14 @@ func main() {
 		log.Error().Err(err)
 	}
 
+	// Prometheus config
+	hist := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "granting",
+		Help: "Number of grant operation",
+	}, []string{"code"})
+
+	prometheus.Register(hist)
+
 	router := mux.NewRouter()
 	router.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
@@ -39,8 +49,9 @@ func main() {
 	router.HandleFunc("/refresh", services.RefreshK8SResources).Methods(http.MethodGet) // TODO, protect from users
 	router.HandleFunc("/config", services.GenerateConfig).Methods(http.MethodGet)
 	router.HandleFunc("/token", services.GenerateJWT).Methods(http.MethodGet)
-	router.HandleFunc("/authenticate", services.Authenticate).Methods(http.MethodPost)
+	router.HandleFunc("/authenticate", services.AuthenticateHandler(hist)).Methods(http.MethodPost)
 
+	router.Handle("/metrics", promhttp.Handler())
 	utils.Log.Info().Msgf(" Preparing to serve request, port: %d", 8000)
 	utils.Log.Fatal().Err(http.ListenAndServeTLS(":8000", utils.TlsCertPath, utils.TlsKeyPath, router))
 
