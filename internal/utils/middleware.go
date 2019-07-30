2d41e1d6d83ce21c961da1e0371ebc76e0f6eb83
Emmanuel Fortin <emmanuel.fortin@ca-silca.fr>
2019-07-30 11:52:00 +0200

    Register histogram for request

--- internal/utils/middleware.go
+++ internal/utils/middleware.go
@@ -6,18 +6,13 @@ import (
 	"time"
 )
 
-// Define our struct
-type authenticationMiddleware struct {
-	tokenUsers map[string]string
-}
-
 func Middleware(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
 		defer func() {
 			httpDuration := time.Since(start)
-			histogram.WithLabelValues(fmt.Sprintf("%s", r.RequestURI)).Observe(httpDuration.Seconds())
+			Histogram.WithLabelValues(fmt.Sprintf("%s", r.RequestURI)).Observe(httpDuration.Seconds())
 		}()
 		next.ServeHTTP(w, r)
 	})
--- internal/utils/prometheus.go
+++ internal/utils/prometheus.go
@@ -10,7 +10,7 @@ var TokenCounter = promauto.NewCounterVec(prometheus.CounterOpts{
 	Help: "Total number of tokens issued",
 }, []string{"status"})
 
-var histogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+var Histogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 	Name:    "kubi_http_requests",
 	Help:    "Time per requests",
 	Buckets: []float64{1, 2, 5, 6, 10}, //defining small buckets as this app should not take more than 1 sec to respond
