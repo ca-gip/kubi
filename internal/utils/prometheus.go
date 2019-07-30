c392af7aa72020eaf666904ef0a219ec984686f6
Emmanuel Fortin <emmanuel.fortin@ca-silca.fr>
2019-07-30 09:49:34 +0200

    Add better metric and histogram vect for kubi

--- internal/utils/prometheus.go
+++ internal/utils/prometheus.go
@@ -14,4 +14,4 @@ var histogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 	Name:    "kubi_http_requests",
 	Help:    "Time per requests",
 	Buckets: []float64{1, 2, 5, 6, 10}, //defining small buckets as this app should not take more than 1 sec to respond
-}, []string{"code", "path"}) // this will be partitioned by the HTTP code.
+}, []string{"path"}) // this will be partitioned by the HTTP code.
