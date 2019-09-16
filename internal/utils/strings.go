1dc75f913e0de0a794ebf258bc7394d4e502c979
Emmanuel Fortin <emmanuel.fortin@ca-silca.fr>
2019-09-16 16:08:26 +0200

    Fix dash !

--- internal/utils/strings.go
+++ internal/utils/strings.go
@@ -5,7 +5,7 @@ import "strings"
 // Test if a string a one of suffixes array present.
 func HasSuffixes(word string, suffixes []string) bool {
 	for _, suffix := range suffixes {
-		if strings.HasSuffix(word, suffix) {
+		if strings.HasSuffix(word, "-"+suffix) {
 			return true
 		}
 	}
