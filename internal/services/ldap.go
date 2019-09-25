e82dc61911928ce45393319bc735aa901ef74247
efortin <emmanuel.fortin@ca-gip.fr>
2019-09-25 23:12:16 +0200

    Kubi parse environment now ! for (dev, prod, pprod, int, uat, development, production, integration and preproduction)

--- internal/services/ldap.go
+++ internal/services/ldap.go
@@ -12,8 +12,8 @@ import (
 var DnsParser = regexp.MustCompile("(?:.+_+)*(?P<namespace>.+)_(?P<role>.+)$")
 
 // Get Namespace, Role for a list of group name
-func GetUserNamespaces(groups []string) []*types.NamespaceAndRole {
-	res := make([]*types.NamespaceAndRole, 0)
+func GetUserNamespaces(groups []string) []*types.Project {
+	res := make([]*types.Project, 0)
 	for _, groupname := range groups {
 		tupple, err := GetUserNamespace(groupname)
 		if err == nil {
@@ -25,8 +25,33 @@ func GetUserNamespaces(groups []string) []*types.NamespaceAndRole {
 	return res
 }
 
+// Parse an ldap namespace an extract:
+// - Kubernetes namespace
+// - Project ( namespace without environment)
+// - Environment
+// If environment not found, return the namespace as is
+func NamespaceParser(namespace string) types.Project {
+	var project = types.Project{}
+
+	if !utils.HasSuffixes(namespace, utils.AllEnvironments) {
+		project.Namespace = namespace
+		project.Project = namespace
+		return project
+	}
+
+	splits := strings.Split(namespace, "-")
+	environment := splits[len(splits)-1]
+	if utils.LdapMapping[environment] != utils.Empty {
+		environment = utils.LdapMapping[environment]
+	}
+	project.Environment = environment
+	project.Project = strings.Join(splits[:len(splits)-1], "-")
+	project.Namespace = fmt.Sprintf("%s-%s", project.Project, project.Environment)
+	return project
+}
+
 // Get Namespace, Role for a group name
-func GetUserNamespace(group string) (*types.NamespaceAndRole, error) {
+func GetUserNamespace(group string) (*types.Project, error) {
 
 	lowerGroup := strings.ToLower(group)
 	keys := DnsParser.SubexpNames()
@@ -46,34 +71,33 @@ func GetUserNamespace(group string) (*types.NamespaceAndRole, error) {
 	}
 
 	//lowerGroup = strings.TrimPrefix(lowerGroup, )
-	namespace, role := DnsParser.ReplaceAllString(lowerGroup, "${namespace}"), DnsParser.ReplaceAllString(lowerGroup, "${role}")
+	raw_namespace, role := DnsParser.ReplaceAllString(lowerGroup, "${namespace}"), DnsParser.ReplaceAllString(lowerGroup, "${role}")
+	project := NamespaceParser(raw_namespace)
+	project.Role = role
 
-	isNamespaceValid, _ := regexp.MatchString(utils.Dns1123LabelFmt, namespace)
+	isNamespaceValid, _ := regexp.MatchString(utils.Dns1123LabelFmt, project.Namespace)
 	isRoleValid, _ := regexp.MatchString(utils.Dns1123LabelFmt, role)
 
-	if utils.Index(utils.BlacklistedNamespaces, namespace) != -1 {
+	if utils.Index(utils.BlacklistedNamespaces, project.Namespace) != -1 {
 		return nil, errors.New(fmt.Sprintf(`
 			LDAP: The ldap group %v, cannot be created. 
-			The namespace %v is protected.`, group, namespace))
+			The namespace %v is protected.`, group, project.Namespace))
 	} else if !isNamespaceValid {
 		return nil, errors.New(fmt.Sprintf(`
 			LDAP: The ldap group %v, cannot be created. 
-			The namespace %v is not dns1123 compliant.`, group, namespace))
+			The namespace %v is not dns1123 compliant.`, group, project.Namespace))
 	} else if !isRoleValid {
 		return nil, errors.New(fmt.Sprintf(`
 			LDAP: The ldap group %v, cannot be created. 
-			The role %v is not dns1123 compliant.`, group, namespace))
-	} else if len(namespace) > utils.DNS1123LabelMaxLength {
+			The role %v is not dns1123 compliant.`, group, project.Namespace))
+	} else if len(project.Namespace) > utils.DNS1123LabelMaxLength {
 		return nil, errors.New(fmt.Sprintf(`
 			LDAP: The name for namespace cannot exceeded %v characters.`, utils.DNS1123LabelMaxLength))
 	} else if len(role) > utils.DNS1123LabelMaxLength {
 		return nil, errors.New(fmt.Sprintf(`
 			LDAP: The name for role cannot exceeded %v characters.`, utils.DNS1123LabelMaxLength))
 	} else {
-		return &types.NamespaceAndRole{
-			Namespace: namespace,
-			Role:      role,
-			Source:    lowerGroup,
-		}, nil
+
+		return &project, nil
 	}
 }
