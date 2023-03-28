ee698a16b2b82299d3c72b4ca4c7a1fd5030d395
y_yanis <yyousnadj@yahoo.fr>
2023-03-28 16:53:37 +0200

    Update test.go

--- .github/test.go
+++ .github/test.go
@@ -34,7 +34,7 @@ func main() {
                 panic(err.Error())
         } 
         // existing ns 
-                nsName := "yanis-test-development"
+                nsName := "chaos-development"
                 _, err = clientset.CoreV1().Namespaces().Get(context.TODO(),nsName, metav1.GetOptions{})
                 if err != nil {
                   if os.IsNotExist(err) {
