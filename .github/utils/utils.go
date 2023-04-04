c3b100bd426bce6328bf8788bd8b8d25803f26ef
y_yanis <yyousnadj@yahoo.fr>
2023-04-04 12:05:54 +0200

    Update utils.go

--- .github/utils/utils.go
+++ .github/utils/utils.go
@@ -11,15 +11,24 @@ here ;)
 package utils
 
 import (
-  
-  
-  "errors"
-	"fmt"
-	"os"
-	"os/exec"
-	"strings"  
-        . "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	
+	
+
+		"context"
+		"flag"
+                "path/filepath"
+		"testing"
+		"errors"
+		"fmt"
+		"os"
+		"os/exec"
+		"strings" 
+	         metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	        "k8s.io/client-go/kubernetes"
+	        "k8s.io/client-go/tools/clientcmd"
+	        "k8s.io/client-go/util/homedir" 
+		. "github.com/onsi/ginkgo/v2"
+		. "github.com/onsi/gomega"
 	
 )
 
@@ -48,32 +57,58 @@ func InstallKubiOperator() error {
 
 func InstallKubiCrd() error {
 
-  url := fmt.Sprintf(kubiCrdUrl)
-  cmd := exec.Command("kubectl","apply","-f",url)
+  cmd := exec.Command("kubectl","apply","-f",kubiCrdUrl)
   -, err := Run(cmd)
   retunr err
 }
 
-func DeployKubiLocalConfig() error {
+func InstallKubilocalconfig() error {
 
-  url := fmt.Sprintf(kubiLocalConfigUrl)
-  cmd := exec.Command("kubectl","apply","-f",url)
+ 
+  cmd := exec.Command("kubectl","apply","-f",kubiLocalConfigUrl)
   -, err := Run(cmd)
   retunr err
 }
 
-func InstallKubiPrerequies() error {
+func InstallKubilocalconfig() error {
 
-  url := fmt.Sprintf(kubiDeploymentUrl)
-  cmd := exec.Command("kubectl","apply","-f",url)
+  cmd := exec.Command("kubectl","apply","-f",kubiPrerequiesUrl)
   -, err := Run(cmd)
   retunr err
 }
 
-func InstallKubiOperator() error {
 
-  url := fmt.Sprintf(kubiPrerequiesUrl)
-  cmd := exec.Command("kubectl","apply","-f",url)
-  -, err := Run(cmd)
-  retunr err
-}
+func TestMain(t *testing.T) {
+	
+	
+    var kubeconfig *string
+    if home := homedir.HomeDir(); home != "" {
+        kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+    } else {
+        kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+    }
+    flag.Parse()
+
+    // use the current context in kubeconfig
+    config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+    if err != nil {
+        t.Fatalf("error building config from flags: %v", err)
+    }
+
+    // create the clientset
+    clientset, err := kubernetes.NewForConfig(config)
+    if err != nil {
+        t.Fatalf("error creating clientset: %v", err)
+    }
+	// install deps
+	// configure cert
+	// configur ldap
+	// deplloy kubi
+	
+	// test
+
+    // existing ns
+    nsName := "chaos-development"
+    _, err = clientset.CoreV1().Namespaces().Get(context.TODO(), nsName, metav1.GetOptions{}) 
+    assert.NoError(t, err, "namespace should exist")
+   
