f41d4dacdb3fe7c56220ca411aa2251313373a7e
y_yanis <yyousnadj@yahoo.fr>
2023-05-16 23:56:33 +0200

    Update Csr.sh

--- .github/Csr.sh
+++ .github/Csr.sh
@@ -161,10 +161,10 @@ kubectl wait --for=condition=Ready pod -n kube-system -l app=kubi-ldap
 POD_NAME=$(kubectl get pods -n kube-system -l app=kubi-ldap -o jsonpath='{.items[0].metadata.name}')
 
 # Installation de ldap-utils
-kubectl exec -n kube-system "$POD_NAME" -- apt-get install -y ldap-utils
+kubectl exec -n kube-system "$POD_NAME" -- sudo apt-get install -y ldap-utils
 
 # Exécution de la commande ldapadd
-kubectl exec -n kube-system "$POD_NAME" -- ldapadd -x -D cn=admin,dc=kubi,dc=ca-gip,dc=github,dc=com -w password <<EOF
+kubectl exec -n kube-system "$POD_NAME" --   ldapadd -x -D cn=admin,dc=kubi,dc=ca-gip,dc=github,dc=com -w password <<EOF
 dn: cn=DL_KUB_CHAOS-DEV_ADMIN,ou=LOCAL,ou=Groups,dc=kubi,dc=ca-gip,dc=github,dc=com
 objectClass: top
 objectClass: groupOfNames
