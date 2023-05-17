2565038c1ce15d6cfe0e20a49689be33d77b1ea1
y_yanis <yyousnadj@yahoo.fr>
2023-05-17 11:09:52 +0200

    Update Csr.sh

--- .github/Csr.sh
+++ .github/Csr.sh
@@ -156,12 +156,9 @@ kubectl wait --for=condition=Ready pod -n kube-system -l app=kubi-ldap
 
 kubectl get all -n kube-system
 
-# Exécution des commandes à l'intérieur du pod kubi-ldap
 
-POD_NAME=$(kubectl get pods -n kube-system -l app=kubi-ldap -o jsonpath='{.items[0].metadata.name}')
-
-kubectl exec "$POD_NAME" -- apt-get update
-kubectl exec "$POD_NAME" -- apt-get install -y ldap-utils
+kubectl exec $(kubectl get pods -n kube-system -l app=kubi-ldap -o jsonpath='{.items[0].metadata.name}')  -- apt-get update
+kubectl exec $(kubectl get pods -n kube-system -l app=kubi-ldap -o jsonpath='{.items[0].metadata.name}')  -- apt-get install -y ldap-utils
 
 
 # Exécution de la commande ldapadd
