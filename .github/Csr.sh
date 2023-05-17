0c26c370a301b921c0b9c9b94cc21ccb699a6d76
y_yanis <yyousnadj@yahoo.fr>
2023-05-17 16:46:37 +0200

    Update Csr.sh

--- .github/Csr.sh
+++ .github/Csr.sh
@@ -155,7 +155,7 @@ sudo ls /var/run/secrets/certs/
 export TERM=xterm
 
 kubectl wait --for=condition=Ready pod -n kube-system -l app=kubi-ldap
-kubectl exec -n kube-system $(kubectl get pods -n kube-system -l app=kubi-ldap -o jsonpath='{.items[0].metadata.name}') -- su -c  " apt-get install -y ldap-utils &&
+kubectl exec -n kube-system $(kubectl get pods -n kube-system -l app=kubi-ldap -o jsonpath='{.items[0].metadata.name}') -- su -c  " apt-get install -y  slapd ldap-utils &&
 ldapadd -x -D  cn=admin,dc=kubi,dc=ca-gip,dc=github,dc=com -w password <<EOF
 dn: cn=DL_KUB_CHAOS-DEV_ADMIN,ou=LOCAL,ou=Groups,dc=kubi,dc=ca-gip,dc=github,dc=com
 objectClass: top
