3dc413ff80bfb2f154258163262f3f67bdaf4c6b
Jean-Philippe Evrard <jean-philippe.evrard-prestataire@ca-gip.fr>
2024-12-19 16:26:08 +0100

    Refactor code to be more idiomatic
    Without this, the naming is a bit hard to follow, and the
    indentation is not really matching the usual go patterns.
    
    This fixes it to make the code more readable.

--- internal/services/webhook-tokenauthenticator.go
+++ internal/services/webhook-tokenauthenticator.go
@@ -15,9 +15,6 @@ import (
 func AuthenticateHandler(issuer *TokenIssuer) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		var code int
-		var tokenStatus error
-
 		bodyString, err := io.ReadAll(r.Body)
 		if err != nil {
 			utils.Log.Error().Err(err)
@@ -28,80 +25,74 @@ func AuthenticateHandler(issuer *TokenIssuer) http.HandlerFunc {
 			utils.Log.Error().Msg(err.Error())
 		}
 
-		token, err := issuer.CurrentJWT(tokenReview.Spec.Token)
-		if err == nil {
-			tokenStatus = issuer.VerifyToken(tokenReview.Spec.Token)
-		}
+		token, err := issuer.VerifyToken(tokenReview.Spec.Token)
 
-		if err != nil || tokenStatus != nil {
+		if err != nil {
 			resp := v1beta1.TokenReview{
 				Status: v1beta1.TokenReviewStatus{
 					Authenticated: false,
 				},
 			}
-			code = http.StatusUnauthorized
-			w.WriteHeader(code)
+			w.WriteHeader(http.StatusUnauthorized)
 			w.Header().Set("Content-Type", "application/json")
 			json.NewEncoder(w).Encode(resp)
-		} else {
-			utils.Log.Info().Msgf("Challenging token for user %v", token.User)
+		}
 
-			var groups []string
-			groups = append(groups, utils.AuthenticatedGroup)
-			groups = append(groups, fmt.Sprintf(utils.KubiClusterRoleBindingReaderName))
+		utils.Log.Info().Msgf("Challenging token for user %v", token.User)
 
-			// Other ldap group are injected
-			for _, auth := range token.Auths {
-				groups = append(groups, fmt.Sprintf("%s-%s", auth.Namespace(), auth.Role))
-			}
+		var groups []string
+		groups = append(groups, utils.AuthenticatedGroup)
+		groups = append(groups, fmt.Sprintf(utils.KubiClusterRoleBindingReaderName))
 
-			groups = append(groups, token.Groups...)
+		// Other ldap group are injected
+		for _, auth := range token.Auths {
+			groups = append(groups, fmt.Sprintf("%s-%s", auth.Namespace(), auth.Role))
+		}
 
-			if token.AdminAccess {
-				groups = append(groups, utils.AdminGroup)
-			}
+		groups = append(groups, token.Groups...)
 
-			if token.OpsAccess {
-				groups = append(groups, utils.OPSMaster)
-			}
+		if token.AdminAccess {
+			groups = append(groups, utils.AdminGroup)
+		}
 
-			if token.ApplicationAccess {
-				groups = append(groups, utils.ApplicationMaster)
-			}
+		if token.OpsAccess {
+			groups = append(groups, utils.OPSMaster)
+		}
 
-			if token.ServiceAccess {
-				groups = append(groups, utils.ServiceMaster)
-			}
+		if token.ApplicationAccess {
+			groups = append(groups, utils.ApplicationMaster)
+		}
 
-			if token.ViewerAccess {
-				groups = append(groups, utils.ApplicationViewer)
-			}
+		if token.ServiceAccess {
+			groups = append(groups, utils.ServiceMaster)
+		}
 
-			resp := v1beta1.TokenReview{
-				Status: v1beta1.TokenReviewStatus{
-					Authenticated: true,
-					User: v1beta1.UserInfo{
-						Username: token.User,
-						Groups:   groups,
-					},
+		if token.ViewerAccess {
+			groups = append(groups, utils.ApplicationViewer)
+		}
+
+		resp := v1beta1.TokenReview{
+			Status: v1beta1.TokenReviewStatus{
+				Authenticated: true,
+				User: v1beta1.UserInfo{
+					Username: token.User,
+					Groups:   groups,
 				},
-			}
-			w.Header().Set("Content-Type", "application/json")
-			code = http.StatusOK
-			w.WriteHeader(code)
+			},
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
 
-			jwtTokenString, marshallError := json.Marshal(resp)
-			if marshallError == nil {
-				utils.Log.Debug().Msgf("%v", string(jwtTokenString))
-			} else {
-				utils.Log.Error().Msgf("Errot serializing json to token review: %s", marshallError.Error())
-			}
+		jwtTokenString, marshallError := json.Marshal(resp)
+		if marshallError == nil {
+			utils.Log.Debug().Msgf("%v", string(jwtTokenString))
+		} else {
+			utils.Log.Error().Msgf("Errot serializing json to token review: %s", marshallError.Error())
+		}
 
-			err = json.NewEncoder(w).Encode(resp)
-			if err != nil {
-				utils.Log.Error().Msg(err.Error())
-			}
-
+		err = json.NewEncoder(w).Encode(resp)
+		if err != nil {
+			utils.Log.Error().Msg(err.Error())
 		}
 
 	}
